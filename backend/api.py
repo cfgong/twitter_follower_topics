bf84f62cbc615f50d0bf6b0d99beda82f64a9c9b
Adam R <adamxx19283@gmail.com>
2019-10-30 15:03:55 -0500

    Import notebook_to_script funciton into api.py to receive tweet data

--- backend/api.py
+++ backend/api.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 import numpy as np
+from notebook_to_script import main
 
 app = Flask(__name__)
 CORS(app)
@@ -19,7 +20,7 @@ def action ():
 
     print("RESULT in api", user)
     # users.insert_one({ "user":user})
-    return "ok"
+    return main()
 
 if __name__ == "__main__":
   app.run(debug=True)
